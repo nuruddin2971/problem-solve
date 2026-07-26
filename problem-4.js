eac8c9ac382cd660047b8ffb782b922c85102148
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-26 17:49:26 +0600

    solved problem-4

---
+++ problem-4.js
@@ -0,0 +1,33 @@
+// Problem-4: FizzBuzz (1 to N)
+// loop through numbers from 1 to n. If a number is divisible by 3, print "Fizz"; if divisible by 5, print "Buzz" if divisible by both, print "FizzBuzz"; otherwise, print the number itself.
+
+// Without function
+
+// let number = 15;
+// for (let i = 1; i <= number; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// With function
+function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(15);
